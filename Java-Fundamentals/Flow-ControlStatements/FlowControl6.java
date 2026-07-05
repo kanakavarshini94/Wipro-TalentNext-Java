db4c19f621bf2331aaf57b041ef68d853b610951
kanakavarshini94 <varikouthrapu8121@gmail.com>
2026-07-05 16:48:20 +0530

    Completed FlowControl Assignment6

---
+++ Java-Fundamentals/Flow-ControlStatements/FlowControl6.java
@@ -0,0 +1,26 @@
+public class FlowControl6 {
+    public static void main(String[] args) {
+
+        if (args.length < 2) {
+            System.out.println("Please provide Gender and Age.");
+            return;
+        }
+
+        String gender = args[0];
+        int age = Integer.parseInt(args[1]);
+
+        if (gender.equals("Female")) {
+            if (age >= 1 && age <= 58) {
+                System.out.println("8.2%");
+            } else if (age >= 59 && age <= 100) {
+                System.out.println("9.2%");
+            }
+        } else if (gender.equals("Male")) {
+            if (age >= 1 && age <= 58) {
+                System.out.println("8.4%");
+            } else if (age >= 59 && age <= 100) {
+                System.out.println("10.5%");
+            }
+        }
+    }
+}
