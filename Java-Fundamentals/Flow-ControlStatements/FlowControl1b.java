50576ec5dda3d4dae58cf496aab275bde5b4f9f5
kanakavarshini94 <varikouthrapu8121@gmail.com>
2026-07-05 13:06:01 +0530

    Completed FlowControl1b Assignment

---
+++ Java-Fundamentals/Flow-ControlStatements/FlowControl1b.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class FlowControl1b {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        if ((num1%10)==(num2%10)){
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+        sc.close();
+
+    }
+}
