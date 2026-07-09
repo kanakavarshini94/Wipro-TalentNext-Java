b5117b3d8f3ede4973a62edc75dc6d157b91783c
kanakavarshini94 <varikouthrapu8121@gmail.com>
2026-07-09 13:48:01 +0530

    Added to packages Assignment

---
+++ Abstraction-Packages-Expections/Packages/Assignment2/Testship.java
@@ -0,0 +1,14 @@
+import com.wipro.automobile.ship.Compartment;
+
+public class TestShip {
+
+    public static void main(String[] args) {
+
+
+        Compartment ship = new Compartment(20.5, 15.5, 10.5);
+
+
+        ship.display();
+
+    }
+}
